041f9c464c6b263eb5e29f527f2fa93d2f28fd4a
PORELARTE <kamranbekirovyz@gmail.com>
2023-07-28 22:30:27 +0400

    Added example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:logarte/logarte.dart';
@@ -28,6 +29,9 @@ class App extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.blueGrey.shade900,
       ),
+      navigatorObservers: [
+        LogarteNavigatorObserver(logarte),
+      ],
       home: const HomePage(),
     );
   }
@@ -41,9 +45,16 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  late final Dio _dio;
+
   @override
   void initState() {
     super.initState();
+
+    _dio = Dio()
+      ..interceptors.add(
+        LogarteDioInterceptor(logarte),
+      );
 
     logarte.attachBackDoorButtonOverlay(
       context: context,
@@ -54,16 +65,90 @@ class HomePageState extends State<HomePage> {
   }
 
   @override
+  void dispose() {
+    _dio.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: const Text(
-          'Logarte Example',
-          style: TextStyle(
-            color: Colors.white,
+        title: const Text('Logarte Example'),
+      ),
+      body: Column(
+        children: [
+          const Divider(),
+          ButtonBar(
+            children: [
+              FilledButton.tonal(
+                onPressed: () async {
+                  await _dio.get('https://jsonplaceholder.typicode.com/posts');
+                },
+                child: const Text('GET'),
+              ),
+              FilledButton.tonal(
+                onPressed: () async {
+                  await _dio.post('https://jsonplaceholder.typicode.com/posts');
+                },
+                child: const Text('POST'),
+              ),
+              FilledButton.tonal(
+                onPressed: () async {
+                  await _dio.put('https://jsonplaceholder.typicode.com/posts');
+                },
+                child: const Text('PUT'),
+              ),
+              FilledButton.tonal(
+                onPressed: () async {
+                  await _dio
+                      .delete('https://jsonplaceholder.typicode.com/posts');
+                },
+                child: const Text('DELETE'),
+              ),
+            ],
           ),
-        ),
+          const Divider(),
+          ButtonBar(
+            children: [
+              FilledButton.tonal(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    routeSettings: const RouteSettings(
+                      name: '/test-dialog',
+                    ),
+                    builder: (BuildContext context) {
+                      return const AlertDialog(
+                        title: Text('Dialog'),
+                        content: Text(
+                          'Opening of this dialog was logged to Logarte',
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: const Text('Open dialog'),
+              ),
+              FilledButton.tonal(
+                onPressed: () {
+                  try {
+                    throw Exception('Exception');
+                  } catch (e, s) {
+                    logarte.error(e, stackTrace: s);
+                  }
+                },
+                child: const Text('Exception'),
+              ),
+              FilledButton.tonal(
+                onPressed: () {
+                  logarte.log('Printed to console');
+                },
+                child: const Text('Plain log'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
