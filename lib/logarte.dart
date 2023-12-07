8abbe8acfa1e18b10fe31f157a2a6df61ddb9c47
PORELARTE <kamranbekirovyz@gmail.com>
2023-12-08 00:00:13 +0400

    Add LogarteMagicalTap widget for detecting taps and showing Logarte widget

--- lib/logarte.dart
+++ lib/logarte.dart
@@ -4,3 +4,4 @@ export 'src/logarte.dart';
 export 'src/models/logarte_entry.dart';
 export 'src/channels/logarte_navigator_observer.dart';
 export 'src/channels/logarte_dio_interceptor.dart';
+export 'src/console/logarte_magical_tap.dart';
---
+++ lib/src/console/logarte_magical_tap.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:logarte/logarte.dart';
+
+/// A widget that detects taps and shows the [Logarte] widget when the user
+class LogarteMagicalTap extends StatefulWidget {
+  /// The widget below this widget in the tree.
+  final Widget child;
+
+  /// How this gesture detector should behave during hit testing.
+  ///
+  /// Defaults to [HitTestBehavior.translucent].
+  final HitTestBehavior behavior;
+
+  /// The [Logarte] instance to show when the user taps the widget.
+  final Logarte logarte;
+
+  /// Creates a new instance of [LogarteMagicalTap].
+  ///
+  /// The [child] and [logarte] arguments are required.
+  const LogarteMagicalTap({
+    Key? key,
+    required this.child,
+    required this.logarte,
+    this.behavior = HitTestBehavior.translucent,
+  }) : super(key: key);
+
+  @override
+  State<LogarteMagicalTap> createState() => _LogarteMagicalTapState();
+}
+
+class _LogarteMagicalTapState extends State<LogarteMagicalTap> {
+  late int _count;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _count = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: widget.behavior,
+      onTap: () {
+        _count++;
+
+        if (_count == 10) {
+          widget.logarte.attach(context: context, visible: true);
+        }
+      },
+      child: widget.child,
+    );
+  }
+}
